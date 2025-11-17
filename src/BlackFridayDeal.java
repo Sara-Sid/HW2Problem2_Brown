84266b1621586504e1b02a7875f39c8bd9302f86
Sara-Sid <s.brown28@ncf.edu>
2025-11-17 15:17:42 -0500

    Implement customer removal from notifications
    Added functionality to remove customers from Black Friday Deals notifications.

--- src/BlackFridayDeal.java
+++ src/BlackFridayDeal.java
@@ -9,10 +9,15 @@ public class BlackFridayDeal {
         this.customer.add(customer);
         System.out.println(customer.getName() + " is now subscribed to Black Friday Deals");
     }
+    // Removing a customer to receive notifications
+    public void removeCustomer(Customer customer){
+        this.customer.remove(customer);
+        System.out.println(customer.getName() + " is now unsubscribed to Black Friday Deals");
+    }
 
-    /* Sending notifications to every customer in the list who is subscribed */
+    // Sending notifications to every customer in the list who is subscribed 
     public void notifyCustomers(String deal){
-        for (Customer customer : this.customer){
+        for (Customer customer: this.customer){
             customer.receiveNotifications(deal);
             System.out.println("------GET IT WHILE IT LASTSSSS-----\n");
         }
