a28414305b2d744dc4825e726825c04c60934b4f
Sara-Sid <s.brown28@ncf.edu>
2025-11-19 22:35:28 -0500

    Refactor customer management in BlackFridayDeal

--- src/BlackFridayDeal.java
+++ src/BlackFridayDeal.java
@@ -2,26 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlackFridayDeal {
-    protected List<Customer> customer = new ArrayList<>();
+    private List<Customer> customers = new ArrayList<>();
 
-    // Adding a customer to receive notifications
-    public void addCustomer(Customer customer){
-        this.customer.add(customer);
-        System.out.println(customer.getName() + " is now subscribed to Black Friday Deals");
-    }
-    // Removing a customer to receive notifications
-    public void removeCustomer(Customer customer){
-        this.customer.remove(customer);
-        System.out.println(customer.getName() + " is now unsubscribed to Black Friday Deals");
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
     }
 
+    public void removeCustomer(Customer customer) {
+        customers.remove(customer);
+    }
 
-    // Sending notifications to every customer in the list who is subscribed 
-    public void notifyCustomers(String deal){
-        for (Customer customer: this.customer){
-            customer.receiveNotifications(deal);
-            System.out.println("------GET IT WHILE IT LASTSSSS-----\n");
+    public void notifyCustomers(String deal, Customer... testList) {
+
+        for (Customer c: testList) {
+            if (customers.contains(c)) {
+                c.receiveNotifications(deal);
+            } else {
+                System.out.println("Customer " + c.getName() + " is not subscribed to deal notifications.");
+            }
         }
     }
-
 }
