1eb565ce61d4888867fcf99ac9678ac31c4b85a3
Sara-Sid <s.brown28@ncf.edu>
2025-11-19 22:46:17 -0500

    Refactor BlackFridayDeal class for better customer handling
    Updated customer management and notification methods.

--- src/BlackFridayDeal.java
+++ src/BlackFridayDeal.java
@@ -1,26 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class BlackFridayDeal {
-    protected List<Customer> customers = new ArrayList<>();
-
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
-
-    public void removeCustomer(Customer customer) {
-        customers.remove(customer);
-    }
-
-    public void notifyCustomers(String deal, List<Customer> allCustomers) {
-        for (Customer c : allCustomers) {
-
-            if (customers.contains(c)) {
-                c.receiveNotification(deal);
-            } 
-            else {
-                System.out.println("Customer " + c.getName() + " is not subscribed to deal notifications.\n");
-            }
-        }
-    }
+ import java.util.ArrayList; 
+import java.util.List; 
+public class BlackFridayDeal { 
+    protected List<Customer> customer = new ArrayList<>();
+    // Adding a customer to receive notifications 
+    public void addCustomer(Customer customer){ 
+        this.customer.add(customer); 
+        System.out.println(customer.getName() + " is now subscribed to Black Friday Deals");
+    } 
+    // Removing a customer to receive notifications
+    public void removeCustomer(Customer customer){ 
+        this.customer.remove(customer); 
+        System.out.println(customer.getName() + " is now unsubscribed to Black Friday Deals"); 
+    } 
+    // Sending notifications to every customer in the list who is subscribed 
+    public void notifyCustomers(String deal){ 
+        for (Customer customer: this.customer){ 
+            customer.receiveNotifications(deal); 
+            System.out.println("------GET IT WHILE IT LASTSSSS-----\n");
+        } 
+    } 
 }
