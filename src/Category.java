8a54648b0ebbedd60d3f1259a46cb9fd6b680cfe
Sara-Sid <s.brown28@ncf.edu>
2025-11-19 22:34:33 -0500

    Refactor Category class to use 'components' list

--- src/Category.java
+++ src/Category.java
@@ -1,27 +1,30 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Category extends ProductComponent{
-    protected List<ProductComponent> component= new ArrayList<>();
-    //Constructor
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category extends ProductComponent {
+    private List<ProductComponent> components = new ArrayList<>();
+
     public Category(String name) {
         super(name, 0);
     }
-    public void add(ProductComponent component){
-        this.component.add(component);
+
+    public void add(ProductComponent component) {
+        components.add(component);
     }
 
     @Override
     public double getPrice() {
         return 0;
     }
+
+    @Override
     public void display() {
         System.out.println("Category: " + getName());
-        for (ProductComponent component : component) {
-            component.display();
-
+        for (ProductComponent c : components) {
+            c.display();
         }
-
     }
-
 }
